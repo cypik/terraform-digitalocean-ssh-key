6049aded1997dd698c85e74059bffe5718be7342
opzbaldev <baldev@opz0.io>
2023-11-24 11:11:51 +0530

    update org name

--- main.tf
+++ main.tf
@@ -1,5 +1,3 @@
-#Module      : SSH KEY
-#Description : Provides a DigitalOcean SSH key resource to allow you to manage SSH keys for Droplet access.
 resource "digitalocean_ssh_key" "default" {
   count = var.enable_ssh_key == true ? 1 : 0
 
